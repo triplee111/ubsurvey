1a341b4a84bfa29f942b43978683c716b1ee9c21
Wilson <weichenworking@cyadmk.com>
2021-01-21 13:33:42 +0800

    update mock construct and change type to promise

--- app/mock/index.ts
+++ app/mock/index.ts
@@ -1,97 +1,37 @@
-const sectionMock = [
+import { Survey } from '@/types'
+
+const surveyData = [
   {
     id: 1,
-    title: '段落標題1',
-    description: '段落敘述1',
-    questions: [
+    qno: 1,
+    content: '題目1',
+    comment: '備註',
+    type: 'radio-box',
+    visible: true,
+    validate: {
+      required: true
+    },
+    opts: [
       {
         id: 1,
-        type: 1, // 1: 單選；2: 複選；3: 簡答；4: 下拉單選
-        subject: '題目1',
-        isRequired: true,
-        needOther: true,
-        options: [
-          '選項11',
-          '選項21',
-          '選項31'
-        ]
+        item: '選項1'
       },
       {
         id: 2,
-        type: 2, // 1: 單選；2: 複選；3: 簡答；4: 下拉單選
-        subject: '題目2',
-        isRequired: true,
-        needOther: true,
-        options: [
-          '選項12',
-          '選項22',
-          '選項32'
-        ]
-      }
-    ]
-  },
-  {
-    id: 2,
-    title: '段落標題2',
-    description: '段落敘述2',
-    questions: [
-      {
-        id: 3,
-        type: 1, // 1: 單選；2: 複選；3: 簡答；4: 下拉單選
-        subject: '題目3',
-        isRequired: false,
-        needOther: false,
-        options: [
-          '選項221',
-          '選項222',
-          '選項223'
-        ]
-      }
-    ]
-  },
-  {
-    id: 3,
-    title: '段落標題3',
-    description: '段落敘述3',
-    questions: [
-      {
-        id: 4,
-        type: 3, // 1: 單選；2: 複選；3: 簡答；4: 下拉單選
-        subject: '題目4',
-        isRequired: true,
-        needOther: false,
-        options: []
-      }
-    ]
-  },
-  {
-    id: 4,
-    title: '段落標題4',
-    description: '段落敘述4',
-    questions: [
+        item: '選項2'
+      },
       {
         id: 5,
-        type: 4, // 1: 單選；2: 複選；3: 簡答；4: 下拉單選
-        subject: '題目5',
-        isRequired: true,
-        needOther: false,
-        options: [
-          '選項41',
-          '選項42',
-          '選項43',
-          '選項44',
-          '選項45',
-          '選項46',
-          '選項47',
-          '選項48',
-          '選項49',
-          '選項40'
-        ]
+        item: '選項3'
+      },
+      {
+        id: 8,
+        item: '選項4'
       }
     ]
   }
 ]
 
-export {
-  sectionMock
-}
+export default new Promise(resolve => {
+  setTimeout(() => resolve(surveyData), 100)
+}) as Promise<Survey>
