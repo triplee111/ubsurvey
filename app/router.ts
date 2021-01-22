21fd4f80c88c23df8353b104dfe4bc1ca3b642db
Wilson <weichenworking@cyadmk.com>
2021-01-22 18:05:58 +0800

    add default router config

--- app/router.ts
+++ app/router.ts
@@ -1,14 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
-import Container from '@/views/SurveyContainer.vue'
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Container
-  }
-]
+const routes: Array<RouteRecordRaw> = []
 
 const router = createRouter({
   history: createWebHistory(process.env.PROJ_ROUTER_BASE),
