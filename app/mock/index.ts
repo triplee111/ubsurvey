a092ecc276a9d031d0fdcc2b00a5bfc1c217f9ca
Wilson <weichenworking@cyadmk.com>
2021-01-25 10:15:36 +0800

    update mock data

--- app/mock/index.ts
+++ app/mock/index.ts
@@ -29,9 +29,38 @@ const surveyData = [
         item: '選項4'
       }
     ]
+  },
+  {
+    id: 1,
+    qno: 1,
+    content: '題目1',
+    comment: '備註',
+    type: 'radio-box',
+    visible: true,
+    validate: {
+      required: true
+    },
+    opts: [
+      {
+        id: 1,
+        item: '選項1'
+      },
+      {
+        id: 2,
+        item: '選項2'
+      },
+      {
+        id: 5,
+        item: '選項3'
+      },
+      {
+        id: 8,
+        item: '選項4'
+      }
+    ]
   }
 ]
 
 export default new Promise(resolve => {
-  setTimeout(() => resolve(surveyData), 100)
+  setTimeout(() => resolve(surveyData), 1000)
 }) as Promise<Survey>
