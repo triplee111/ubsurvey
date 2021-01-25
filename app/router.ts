a2bd34f97690135ed0446a5d050de31afc9e69f9
Wilson <weichenworking@cyadmk.com>
2021-01-25 10:13:31 +0800

    set default empty route

--- app/router.ts
+++ app/router.ts
@@ -1,6 +1,17 @@
+import { h } from 'vue'
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
-const routes: Array<RouteRecordRaw> = []
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'survey',
+    component: {
+      setup() {
+        return () => h('span')
+      }
+    }
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.PROJ_ROUTER_BASE),
