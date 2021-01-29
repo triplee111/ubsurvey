e76b80b41de8ac81eebc3f86b224dd82a09275a5
Wilson <weichenworking@cyadmk.com>
2021-01-29 20:22:18 +0800

    update subject element components

---
+++ app/survey/subject/element/SubjectLayout.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+section
+  slot(name="section")
+
+    slot(name="before")
+
+    .questionBlock
+      slot(name="question")
+
+    .helperBlock
+      slot(name="helper")
+
+    .optionBlock(v-if="$slots.option")
+      slot(name="option")
+
+    slot(name="after")
+
+</template>
+
+<style lang="stylus" scoped>
+section
+  line-height 2
+
+  .questionBlock
+    color #424343
+
+  .helperBlock
+    color #d43b27
+    font-size 0.85rem
+
+  .optionBlock
+    display flex
+    align-items center
+    justify-content flex-start
+    flex-wrap wrap
+    width 100%
+</style>
---
+++ app/survey/subject/element/SubjectOptions.vue
@@ -0,0 +1,78 @@
+<template lang="pug">
+.optionBlock(
+  v-if="config.optsUi === 'radiobox'"
+  v-bind="$attrs")
+  .options(
+    v-for="(opt, key) in opts"
+    :key="`opt-${key + 1}`")
+    input(
+      v-model="selected"
+      :id="`radio-${key + 1}`"
+      :value="opt.id"
+      type="radio")
+    label(:for="opt.item") {{ opt.item }}
+
+  //- TODO: OTHERS
+  //- .options(v-if="config.others")
+  //-   input#others-input(type="text")
+
+.optionBlock(
+  v-else-if="config.optsUi === 'menu'"
+  v-bind="$attrs")
+  select.menu(v-model="selected")
+    option(
+      v-for="(opt, key) in opts"
+      :key="`opt-${key + 1}`"
+      :value="opt.id")
+      | {{ opt.item }}
+
+  //- TODO: OTHERS
+  //- .options(v-if="config.others")
+  //-   input#others-input(type="text")
+
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref } from 'vue'
+
+import { Option, SubjectConfig } from '@/types'
+
+export default defineComponent({
+  name: 'SubjectOptions',
+  props: {
+    opts: {
+      type: Object as PropType<Option[]>,
+      default: []
+    },
+    config: Object as PropType<SubjectConfig>
+  },
+  setup() {
+    return {
+      selected: ref(0)
+    }
+  }
+})
+</script>
+
+<style lang="stylus" scoped>
+.optionBlock
+  display flex
+  align-items center
+  justify-content flex-start
+  flex-wrap wrap
+  width 100%
+
+  .options
+    padding-right: 20px
+
+    > input
+      margin-right: 5px
+
+      &[type="text"]
+        border-bottom: 1px solid #424343
+        margin-left: 5px
+
+  .menu
+    width: 150px
+    border-radius: 4px
+</style>
