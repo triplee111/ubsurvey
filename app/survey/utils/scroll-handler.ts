ec3582f3fbd9b76fa3111d8708d5d37dbf981741
裕介 <20190808@cyadmk.com>
2021-02-08 12:02:54 +0800

    adjust scroll handler

--- app/survey/utils/scroll-handler.ts
+++ app/survey/utils/scroll-handler.ts
@@ -1,8 +1,13 @@
 const useScrollHandler = () => {
+  const ps = document.querySelector('.scrollContainer') as HTMLElement
+  const psRect = ps.getBoundingClientRect()
+
   const scrollTo = (qid: string) => {
     const subject = document.querySelector(`#question-${qid}`) as HTMLElement
-    if(subject) {
-      subject.scrollIntoView()
+    const subjectRect = subject.getBoundingClientRect()
+    const diffTop = subjectRect.top - psRect.top
+    if(diffTop >= 0) {
+      ps.scrollTop = diffTop
     }
   }
   return {
