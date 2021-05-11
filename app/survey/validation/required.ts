d17c867415e3c3a33a5e9596c2316d48a76de7c4
Wilson <weichenworking@cyadmk.com>
2021-05-11 09:53:07 +0800

    fix required rule

--- app/survey/validation/required.ts
+++ app/survey/validation/required.ts
@@ -1,25 +1,26 @@
 import { IVerify, ValidatorOptions } from '@/types'
 
-export default (
-  fn: IVerify,
-  opts: ValidatorOptions,
-  config?: boolean
-): IVerify => {
+export default (fn: IVerify, opts: ValidatorOptions, config?: boolean): IVerify => {
   return (ans, errors = []) => {
     // break 開啟時，已有驗證錯誤就直接回傳
     if (opts.break && errors.length > 0) {
       return fn(ans, errors)
     }
 
-    // 驗證規則
-    if (
-      ((typeof ans.inputs === 'string' && ans.inputs === '') ||
-        (Array.isArray(ans.select) && ans.select?.length === 0)) &&
-      config
-    ) {
-      errors?.push({ rule: 'required' })
+    if (config) {
+      // 文字輸入題型
+      if (!ans.select && typeof ans.inputs === 'string' && ans.inputs === '') {
+        errors?.push({ rule: 'required' })
 
-      return fn(ans, errors)
+        return fn(ans, errors)
+      }
+
+      // 選擇題型
+      if (Array.isArray(ans.select) && ans.select?.length === 0) {
+        errors?.push({ rule: 'required' })
+
+        return fn(ans, errors)
+      }
     }
 
     return fn(ans, errors)
